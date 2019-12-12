1dc77db997e7de4a1598009f9c14a05443b2df53
Lukas Grajewsky <grajewski.lukasz@wp.pl>
2019-12-12 20:48:10 +0100

    Add required plugin annotations

--- src/Annotations/RequiredAnnotation.php
+++ src/Annotations/RequiredAnnotation.php
@@ -1,13 +1,43 @@
 <?php 
 namespace JsonMapper\Annotations;
 
+use JsonMapper\Interfaces\ValidationAnnotation;
+
 /** 
  * @Annotation
  */
-final class RequiredAnnotation 
+final class RequiredAnnotation implements ValidationAnnotation
 {
+
+    private $errors = [];
+
+
     /** 
-     * @var bool 
+     * @param mixed $value
+     * 
+     * @return bool
      */
-    public $value = true;
+    public function validate($value): bool 
+    {
+        if (is_null($value) || empty($value)) {
+            return false;
+        }
+        return true;
+    }
+    /** 
+     * 
+     * @return string[]
+     */
+    public function errors(): array 
+    {
+        return $this->errors;
+    }
+    /** 
+     * 
+     * @return bool
+     */
+    public function isValid(): bool 
+    {
+        return count($this->errors()) > 0;
+    }
 }
