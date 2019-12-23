f2ad1dbbb1ae711eff8c8ad5cbfb987e19c67258
Lukas Grajewsky <grajewski.lukasz@wp.pl>
2019-12-23 20:28:29 +0100

    add formating

--- functions.php
+++ functions.php
@@ -7,25 +7,29 @@ use JsonMapper\Interfaces\ValidationAnnotation;
 use JsonMapper\JsonMapper;
 
 if (!function_exists("json_mapper_config")) {
-    function json_mapper_config(): Config {
+    function json_mapper_config(): Config 
+    {
         return new Config();
     }
 }
 
 if (!function_exists("json_mapper_container")) {
-    function json_mapper_container(): Container {
+    function json_mapper_container(): Container 
+    {
         return Container::instance();
     }
 }
 
 if (!function_exists("json_mapper")) {
-    function json_mapper(): JsonMapperInterface {
+    function json_mapper(): JsonMapperInterface 
+    {
         return new JsonMapper();
     }
 }
 
 if (!function_exists("json_mapper_validation_plugin")) {
-    function json_mapper_validation_plugin(ValidationAnnotation $validationPlugin): JsonMapperInterface {
-        // TO-DO
+    function json_mapper_validation_plugin(ValidationAnnotation $validationPlugin)
+    {
+        
     }
 }
